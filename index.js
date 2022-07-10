da5c925577145057b2d31116ba9eea655548c1bd
Maria Coitinho <brasildu@gmail.com>
2022-07-10 18:05:18 -0500

    commit tree 97551b069be8bcb2ce2ad1af6b98fc8d8712395a

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// // Code your solution here
+
+// const products = [
+//     { name: 'Shampoo', price: 4.99 },
+//     { name: 'Donuts', price: 7.99 },
+//     { name: 'Cookies', price: 6.49 },
+//     { name: 'Bath Gel', price: 13.99 }
+//   ];
+
+//   function getTotalAmountforProducts(array) {
+//     let total = 0    
+//     for (const element of array) {
+//             total += element.price;
+//         }
+//         return total;
+//   }
+
+// // console.log(getTotalAmountforProducts(products))
+
+// const couponLocations = [
+//     { room: 'Living room', amount: 5 },
+//     { room: 'Kitchen', amount: 2 },
+//     { room: 'Bathroom', amount: 1 },
+//     { room: 'Master bedroom', amount: 7 }
+//   ];
+
+
+// function ourReduce(arr, reducer, init) {
+//     let accum = init;
+//     for (const element of arr) {
+//         accum = reducer(accum, element)
+//     }
+//     return accum;
+// }
+
+// function couponCounter(totalAmount, location) {
+//     return totalAmount + location.amount;
+// }
+
+// // console.log(ourReduce(couponLocations, couponCounter, 0))
+
+
+// // console.log(couponLocations.reduce(couponCounter, 0))
+
+// const doubleAndSummed = [1,2,3,4].reduce((accum, num) => {
+//     return accum + num * 2})
+
+// // console.log(doubleAndSummed)
+
+// const addAll = [1,2,3,4].reduce((accum, element) => {
+//     return accum + element;
+// })
+// console.log(addAll)
+
+
+
+// --------------------- LAB
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((element, accum) => {
+    return accum + element;
+}, 0);
+
+console.log(totalBatteries)
